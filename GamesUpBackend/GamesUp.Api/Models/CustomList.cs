630e3626b3bd212306e4fdd6289d6cb4df9f80e8
ubidu <kniechcial2001@gmail.com>
2023-10-24 00:16:30 +0200

    added models

---
+++ GamesUpBackend/GamesUp.Api/Models/CustomList.cs
@@ -0,0 +1,9 @@
+﻿namespace GamesUp.Models;
+
+public class CustomList
+{
+    public int ID { get; set; }
+    public string Name { get; set; }
+    public User User { get; set; }
+    public ICollection<Game> Games { get; set; }
+}
--- GamesUpBackend/GamesUp.Api/Models/Game.cs
+++ GamesUpBackend/GamesUp.Api/Models/Game.cs
@@ -5,5 +5,12 @@ public class Game
     public int ID { get; set; }
     public string Name { get; set; }
     public string Description { get; set; }
-    
+    public string CoverPath { get; set; }
+    public Category Category { get; set; }
+    public DateTime ReleaseDate { get; set; }
+    public Platform Platform { get; set; }
+    public Developer Developer { get; set; }
+    public Publisher Publisher { get; set; }
+    public ICollection<Review> Reviews { get; set; }
+    //Pomyslec nad sensem: Fabula, Technologia, Mechanika
 }
